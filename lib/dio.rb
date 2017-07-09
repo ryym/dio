f7719282142c1f1b6d32fb64fd46f45b05cc7888
ryym <ryym.64@gmail.com>
2017-07-09 09:27:14 +0900

    refac(dio): Remove class variable completely

--- lib/dio.rb
+++ lib/dio.rb
@@ -2,16 +2,18 @@
 
 require 'dio/container'
 
-# rubocop:disable Style/ClassVars
-
 # Dio provides DI functionality.
 module Dio
   extend ActiveSupport::Concern
 
-  @@container = Dio::Container.new
+  @container = Dio::Container.new
 
   def self.inject(obj)
-    @@container.inject(obj)
+    @container.inject(obj)
+  end
+
+  def self.default_container
+    @container
   end
 
   def __dio_inject__(container)
@@ -21,7 +23,7 @@ module Dio
   class_methods do
     def be_injectable
       # TODO: User can define the key and its factory block.
-      @@container.register(self) { |*args| new(*args) }
+      Dio.default_container.register(self) { |*args| new(*args) }
     end
 
     def inject(&injector)
